8e7feadd4503d72daeedbdc6b96236b6fcb481fa
Ian Pollack <ianjpollack@gmail.com>
2018-03-30 18:19:09 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,3 +17,9 @@ def help
   - play : lets you choose a song to play
   - exit : exits this program"
 end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
